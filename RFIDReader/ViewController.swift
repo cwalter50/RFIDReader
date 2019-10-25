9abce83aaa034f28f201828cc9fe4fc7fdddca8c
cwalter50 <walt50@gmail.com>
2019-10-25 14:46:39 -0400

    Initial Commit

---
+++ RFIDReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RFIDReader
+//
+//  Created by  on 10/25/19.
+//  Copyright © 2019 DocsApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
